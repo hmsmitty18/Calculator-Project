a3e2b6a6381872790b3e5ee022a73db7e46db5ca
hmsmitty18 <hmsmitty111898@gmail.com>
2014-07-24 14:54:00 -0400

    finished the calculator

---
+++ calculator.rb
@@ -0,0 +1,44 @@
+puts "What do you want your first value to be"
+
+number1 = gets.chomp.to_i
+
+
+puts  "What operation do you want(+, -, /, *,)"
+
+symbol = gets.chomp
+
+
+puts "What do you want your second value to be"
+
+number2 = gets.chomp.to_i
+
+
+def adds(first_number, second_number)
+	puts first_number + second_number
+end
+
+def subtract(first_number, second_number)
+	puts first_number - second_number
+end
+	
+def multiply(first_number, second_number)
+	puts first_number * second_number
+end
+
+def divide(first_number, second_number)
+	puts first_number / second_number
+end
+
+
+
+if symbol == "+"
+	adds(number1, number2)
+elsif symbol == "*" 
+	multiply(number1, number2)
+elsif symbol == "/"
+	divide(number1, number2)
+else symbol == "-"
+	subtract(number1, number2)
+end
+
+
